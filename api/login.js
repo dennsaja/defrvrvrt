b444f4744bb4420a60a1ed8e98c6132a21ad319e
Denis <136689391+dennsaja@users.noreply.github.com>
2026-04-02 16:16:57 +0700

    Update login.js

--- api/login.js
+++ api/login.js
@@ -8,26 +8,34 @@ const supabase = createClient(
 );
 
 export default async function handler(req, res) {
-  const { username, password } = req.body;
+  try {
+    console.log("BODY:", req.body);
+     const { username, password } = req.body;
 
-  const { data, error } = await supabase
-    .from('users')
-    .select('*')
-    .eq('username', username)
-    .single();
+     const { data, error } = await supabase
+       .from('users')
+       .select('*')
+       .eq('username', username)
+       .single();
 
-  if (error || !data)
-    return res.status(401).json({ error: 'User tidak ditemukan' });
+     if (error || !data)
+       return res.status(401).json({ error: 'User tidak ditemukan' });
 
-  const valid = await bcrypt.compare(password, data.password);
-  if (!valid)
-    return res.status(401).json({ error: 'Password salah' });
+     const valid = await bcrypt.compare(password, data.password);
+     if (!valid)
+       return res.status(401).json({ error: 'Password salah' });
 
-  const token = jwt.sign(
-    { id: data.id, username: data.username },
-    process.env.JWT_SECRET,
-    { expiresIn: '7d' }
+     const token = jwt.sign(
+       { id: data.id, username: data.username },
+       process.env.JWT_SECRET,
+       { expiresIn: '7d' }
   );
 
   res.json({ token, user: data });
+  res.status(200).json({ message: "OK" });
+
+  } catch (err) {
+    console.error("LOGIN ERROR:", err);
+    res.status(500).json({ message: err.message });
+  }
 }
